3c8c1e298f9b6017e935f7f49a08b9c466ce14c7
yourajkindamid <aliyouraj47@gmail.com>
2025-10-18 01:26:59 +0500

    Commenting out the BlockChain class functionality as not currently required

--- Deliverable_2/linked list testing/linkedlistimpel.cpp
+++ Deliverable_2/linked list testing/linkedlistimpel.cpp
@@ -16,6 +16,7 @@ struct Block {
 };
 
 
+// THIS CLASS IS GOING TO BE IMPLEMENTED POST DELIVERABLE 2
 //working with a class makes it easier to connect all the Blocks (i.e nodes) hence fullfilling the purpose of the linked list and blockchain
 class BlockChain
 {
@@ -24,67 +25,87 @@ class BlockChain
     Block* head;            //the first node should always be assigned to head
 
     public:
-    static int amountofblocks;  //updating index for each block
     BlockChain()    //CONSTRUCTOR
     {
         this->previous = NULL;
         this->head = NULL;
     }
+};
 
-    string noncemaker() //generates a 6 digit integer that can be used as a nonce
+
+static int amountofblocks;  //updating index for each block
+
+
+string noncemaker() //generates a 6 digit integer that can be used as a nonce
+{
+    srand(time(0));
+    int number = rand() % 900000 + 100000;
+    return to_string(number);
+}
+
+
+string hashmaker(string ab)
+{
+    //convert nonce(string) to a random value so that we can hash it
+    int val = stoi(ab) * 37 + 13;     //stoi is a string to integer converter
+
+    for(int i=0; i<6; i++) //6 character hash
     {
-        srand(time(0));
-        int number = rand() % 900000 + 100000;
-        return to_string(number);
+        char base = 'A' + (stoi(ab) % 10);  //the first part of the hash will keep changing b/w 'A' & 'J'
+        char postbase = base + (val % 26);  // sample functin: A + 0-25(A-Z) = AA/AB....
+        ab += postbase;  //adds postbase to the string hash
+        val = val / 3 + i * 7; //changes the value of val for the next iteration
     }
 
+    return ab;
+}
 
-    string hashmaker(string ab)
-    {
-        //convert nonce(string) to a random value so that we can hash it
-        int val = stoi(ab) * 37 + 13;     //stoi is a string to integer converter
 
-        for(int i=0; i<6; i++) //6 character hash
-        {
-            char base = 'A' + (stoi(ab) % 10);  //the first part of the hash will keep changing b/w 'A' & 'J'
-            char postbase = base + (val % 26);  // sample functin: A + 0-25(A-Z) = AA/AB....
-            ab += postbase;  //adds postbase to the string hash
-            val = val / 3 + i * 7; //changes the value of val for the next iteration
-        }
+Block* transaction(double money)
+{
+    Block* ptr = new Block;
 
-        return ab;
-    }
+    ptr->fee = money;
+    ptr->next = NULL; //new node so the next node should always be null
 
-    Block* transaction(double money)
-    {
-        //PLEASE CONDUCT ALL VERIFICATIONS IN MAIN
+    ptr->prevHash = hashmaker(noncemaker());        //we dont need a previous nonce when starting the blockchain
+    ptr->fee = money;
+    ptr->nonce = noncemaker();
+    ptr->hash = hashmaker(ptr->nonce);
 
-        // ptr->next = NULL;   //new node so next is null
-        // cout << "Please provide your public key: ";
-        // getline(cin, pub);
+    cout << "First hash: " << ptr->hash << endl;
 
-        // cout << endl;
+    ptr->index = amountofblocks;
+    cout << "Index number for the transaction: " << ptr->index << endl;
 
-        // //cout << "Please provide your private key for added protection: " verify this in the main file as verification is not possible here
+    amountofblocks++; //has been increase as a new one has been added;
 
-        // cout << "Please provide the reciever's public key: ";
-        // getline(cin, sender);
+    return ptr; //function changed from void to Block* so that this function can be implemented in other files 
+}
 
-        // cout << endl;
 
-        // cout << "Please enter the fee: ";
-        // cin >> ptr->fee;
 
-        // cout << endl;
 
-        //PLEASE CONDUCT ALL VERIFICATIONS IN MAIN SO THAT ONLY THE BLOCKCHAIN/LINKED LIST IS DEALT WITH HERE
 
-        Block* ptr = new Block;
 
-        ptr->fee = money;
-        ptr->next = NULL; //new node so the next node should always be null
-        
-        if(previous != NULL)    //if the BlockChain is continuting
+
+        // //if we want to use sha256 for hashing purposes we will need to download the openssl library and make a specific function to use sha256 (too much work)
+
+    // //using a SHA256 implementation file as a header and using the functions provided in it
+    // void hashmaker(struct Block* ptr)
+    // {
+    //     SHA256 sha;     //object for the header file
+    //     sha.update(ptr->nonce);         //hashing the nonce value
+
+    //     //digest() is a function in the SHA256.h file for computing the hash
+    //     ptr->hash = SHA256::toString(sha.digest());     //toString() converts it into a string form                                               
+    // }
+
+
+
+        // TO BE IMPLEMENTED DURING MINING PHASE (POST DELIVERABLE 2)
+
+        /*    if(previous != NULL)    //if the BlockChain is continuting
         {
             previous = head;
             head = ptr;
@@ -102,30 +123,4 @@ class BlockChain
         if(previous == NULL) //if the blockchain is just starting
         {
             head = ptr;
-            previous = head;
-
-            ptr->prevHash = hashmaker(noncemaker());        //we dont need a previous nonce when starting the blockchain
-            ptr->fee = money;
-            ptr->nonce = noncemaker();
-            ptr->hash = hashmaker(ptr->nonce);
-            cout << "First hash: " << ptr->hash << endl;
-        }
-
-        ptr->index = amountofblocks;
-        cout << "Index number for the transaction: " << ptr->index << endl;
-        amountofblocks++; //has been increase as a new one has been added;
-        return ptr; //function changed from void to Block* so that this function can be implemented in other files 
-    }
-};
-
-        // //if we want to use sha256 for hashing purposes we will need to download the openssl library and make a specific function to use sha256 (too much work)
-
-    // //using a SHA256 implementation file as a header and using the functions provided in it
-    // void hashmaker(struct Block* ptr)
-    // {
-    //     SHA256 sha;     //object for the header file
-    //     sha.update(ptr->nonce);         //hashing the nonce value
-
-    //     //digest() is a function in the SHA256.h file for computing the hash
-    //     ptr->hash = SHA256::toString(sha.digest());     //toString() converts it into a string form                                               
-    // }
+            previous = head; */
